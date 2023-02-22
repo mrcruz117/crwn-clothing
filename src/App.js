7bc3bdd6224cdb9d84dd1a19ecadfc92231d5cc2
mrcruz117 <mrcruz1989@gmail.com>
2023-02-22 16:06:01 -0500

    added navbor styling and logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route,  } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/home/navigation/navigation.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>Nav Bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 const Shop = () => {
   return <h1>Shop</h1>;
 };
