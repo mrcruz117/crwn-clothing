0b03fcdfd7c5a35aec3fa7809920d178f31a42bf
mrcruz117 <mrcruz1989@gmail.com>
2023-02-22 12:12:16 -0500

    converted stubs to mapable component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
 const App = () => {
+  const categories = [
+    {
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      id: 1,
+      linkUrl: "shop/hats",
+    },
+    {
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      id: 2,
+      linkUrl: "shop/jackets",
+    },
+    {
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      id: 3,
+      linkUrl: "shop/sneakers",
+    },
+    {
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
+      id: 4,
+      linkUrl: "shop/womens",
+    },
+    {
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      id: 5,
+      size: "large",
+      linkUrl: "shop/mens",
+    },
+  ];
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map(({ title, imageUrl, id, linkUrl }) => (
+        <div key={id} className="category-container">
+          <img alt={title} src={imageUrl} />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Men</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
