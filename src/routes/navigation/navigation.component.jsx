fef698b987797784211647bcb9b82d7a81dbab8b
mrcruz117 <mrcruz1989@gmail.com>
2023-02-22 17:09:48 -0500

    added sign in component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
-import { ReactComponent as Logo } from "../../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./navigation.styles.scss";
 
@@ -15,6 +15,9 @@ const Navigation = () => {
         <div className="nav-links-container">
           <Link className="nav-link" to={"/shop"}>
             SHOP
+          </Link>
+          <Link className="nav-link" to={"/sign-in"}>
+            SIGN IN
           </Link>
         </div>
       </div>
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,3 @@
+const SignIn = () => <div className="sign-in">signIn</div>;
+
+export default SignIn;
