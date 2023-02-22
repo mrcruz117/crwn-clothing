2da237be3fc8c9a2541537aa4fdf6f8c72889ba0
mrcruz117 <mrcruz1989@gmail.com>
2023-02-22 12:28:11 -0500

    applied styles

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
+import React from "react";
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
       id: 1,
-      linkUrl: "shop/hats",
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
       id: 2,
-      linkUrl: "shop/jackets",
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
       id: 3,
-      linkUrl: "shop/sneakers",
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      size: "large",
       id: 4,
-      linkUrl: "shop/womens",
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
       id: 5,
-      size: "large",
-      linkUrl: "shop/mens",
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, imageUrl, id, linkUrl }) => (
+      {categories.map(({ title, imageUrl, id }) => (
         <div key={id} className="category-container">
-          <img alt={title} src={imageUrl} />
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
