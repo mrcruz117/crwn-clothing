cd1f4aff39b16ca1f14d29ff20b301a5790fb252
mrcruz117 <mrcruz1989@gmail.com>
2023-03-01 11:28:49 -0500

    added cart item styles and add cart functionality also removes dropdown on clickaway

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,35 @@
-import Button from "../button/button.component";
+import { useContext, useRef, useEffect } from "react";
 
+import { CartContext } from "../contexts/cart.context";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+  const { setIsCartOpen, cartItems } = useContext(CartContext);
+
+  const wrapperRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setIsCartOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items" />
+    <div ref={wrapperRef} className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => {
+          return <CartItem key={cartItem.id} cartItem={cartItem} />;
+        })}
+      </div>
       <Button>CHECKOUT</Button>
     </div>
   );
--- src/components/contexts/cart.context.jsx
+++ src/components/contexts/cart.context.jsx
@@ -3,14 +3,38 @@ import { createContext, useState } from "react";
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    const existingItem = cartItems.find(
+      (cartItem) => cartItem.id === productToAdd.id
+    );
+
+    if (existingItem) {
+      setCartItems(
+        cartItems.map((cartItem) =>
+          cartItem.id === productToAdd.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...productToAdd, quantity: 1 }]);
+    }
+  };
+
   const value = {
     isCartOpen,
     setIsCartOpen,
+    addItemToCart,
+    cartItems,
   };
-  
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
