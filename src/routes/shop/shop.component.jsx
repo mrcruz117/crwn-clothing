380be38d11320b0f96c3dbe9521ec18fc1ed6e7a
mrcruz117 <mrcruz1989@gmail.com>
2023-03-03 09:14:30 -0500

    refactored shop page to preview items

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,31 +1,21 @@
-import { Fragment, useContext } from "react";
+import { useContext } from "react";
 import { CategoriesContext } from "../../components/contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 import "./shop.styles.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <>
+    <div className="shop-container">
       {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
         return (
-          <Fragment key={title}>
-            <h1>{title}</h1>
-            <div className="products-container">
-              {categoriesMap[title].map((product) => {
-                return <ProductCard key={product.id} product={product} />;
-              })}
-            </div>
-          </Fragment>
+          <CategoryPreview key={title} title={title} products={products} />
         );
       })}
-      <div className="products-container">
-        {/* {products.map((product) => {
-        return <ProductCard key={product.id} product={product} />;
-      })} */}
-      </div>
-    </>
+    </div>
   );
 };
 
