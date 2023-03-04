2a3e7e2013bfca459ae8092b806ddc8a0537c3fa
mrcruz117 <mrcruz1989@gmail.com>
2023-03-04 17:15:39 -0500

    styled-components to button

--- src/components/sign-in-form/sign-in-form.components.jsx
+++ src/components/sign-in-form/sign-in-form.components.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 
 import {
-  createUserDocumentfromAuth,
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import "./sign-in-form.styles.scss";
 
@@ -80,7 +79,7 @@ const SignInForm = () => {
           <Button type={"submit"}>Sign In</Button>
           <Button
             type="button"
-            buttonType={"google"}
+            buttonType={BUTTON_TYPE_CLASSES.google}
             onClick={signInWithGoogle}
           >
             Google Sign In
