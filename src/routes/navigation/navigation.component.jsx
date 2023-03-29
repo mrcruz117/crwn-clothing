fdf2471e419a2b44e630245aae727a3dadfd1886
mrcruz117 <mrcruz1989@gmail.com>
2023-03-29 10:55:01 -0400

    added currentUser selector.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,9 +15,11 @@ import {
   NavLink,
   LogoContainer,
 } from "./navigation.styles";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
   return (
