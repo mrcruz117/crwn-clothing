e1e5d7a0e778397719e80717ce19366f795f6d68
mrcruz117 <mrcruz1989@gmail.com>
2023-04-04 16:51:02 -0400

    redux-thunk init applied thunk to categories data-fetch

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -7,9 +8,10 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
