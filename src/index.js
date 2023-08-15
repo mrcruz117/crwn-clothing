bb93c1caa0523279f9a79ccb33641cd686ae88f0
Michael Cruz <mrcruz1989@gmail.com>
2023-08-15 15:47:58 -0400

    installed and imported stripe with key

--- src/index.js
+++ src/index.js
@@ -9,9 +9,15 @@ import { PersistGate } from "redux-persist/integration/react";
 // import { CartProvider } from "./components/contexts/cart.context";
 import { store, persistor } from "./store/store";
 
-import "./index.scss";
+import { Elements } from "@stripe/react-stripe-js";
+
 import App from "./App";
+
+import { stripePromise } from "./utils/stripe/stripe.utils";
+
+
 import reportWebVitals from "./reportWebVitals";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -22,7 +28,9 @@ root.render(
           {/* <UserProvider> */}
           {/* <CategoriesProvider> */}
           {/* <CartProvider> */}
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
           {/* </CartProvider> */}
           {/* </CategoriesProvider> */}
           {/* </UserProvider> */}
